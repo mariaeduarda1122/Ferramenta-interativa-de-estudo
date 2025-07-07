4e4a49f8d74128e519d463f9b7cb42aa39167b7b
mariaeduarda1122 <146454198+mariaeduarda1122@users.noreply.github.com>
2025-07-07 08:42:10 -0300

    DUDA

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+perguntas js
+criaCartao(
+    'Sustentabilidade e moda consciente',
+    'Quais são as vantagens de escolher roupas feitas de materiais orgânicos?',
+    'Como a moda consciente ajuda o meio ambiente?'
+)
+
+criaCartao(
+    'Estilo com propósito',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
